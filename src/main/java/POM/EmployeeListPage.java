f5f450684e5b1039c02078775ec20343a55b9703
Subhadeep403 <subhadeepdas950@gmail.com>
2023-08-11 16:34:46 +0530

    2nd commit

--- src/main/java/POM/EmployeeListPage.java
+++ src/main/java/POM/EmployeeListPage.java
@@ -7,52 +7,51 @@ import org.openqa.selenium.support.PageFactory;
 
 public class EmployeeListPage {
 
+	public EmployeeListPage(WebDriver driver) {
 
-	public EmployeeListPage(WebDriver driver)
-	{
-	
 		PageFactory.initElements(driver, this);
 	}
-	
-	@FindBy(xpath="(//input[@placeholder='Type for hints...'])[1]")
+
+	@FindBy(xpath = "(//input[@placeholder='Type for hints...'])[1]")
 	private WebElement employee_Name;
-	
-	@FindBy(xpath="//button[text()=' Search ']")
+
+	@FindBy(xpath = "//button[text()=' Search ']")
 	private WebElement search;
-	
-	@FindBy(xpath="//div[@class='oxd-table-cell oxd-padding-cell'][3]")
+
+	@FindBy(xpath = "//div[@class='oxd-table-cell oxd-padding-cell'][3]")
 	private WebElement verifyName;
-	
-	
-	//getter method
-		public WebElement getSearch() {
-			return search;
-		}
-			public WebElement getEmployee_Name() {
-				return employee_Name;
-			}
-				public WebElement getVerifyName() {
-					return verifyName;
-				}
-				
-				//Logic
-				
-				public void clickSearch(WebDriver driver) {
-					search.click();
-				}
-				
-				public void enterEmployeeName(String emp) {
-					employee_Name.sendKeys(emp);
-				}
-				public void verifyEmployeeName(String emp) {
-					
-					String empName = verifyName.getText();
-					if (emp.contains(empName))
-						System.out.println("Employee Verified  ....PASS");
-				
-					else
-						System.out.println("Employee Not Found ....FAIL");
-				}
-				
-	
+
+	// getter method
+	public WebElement getSearch() {
+		return search;
+	}
+
+	public WebElement getEmployee_Name() {
+		return employee_Name;
+	}
+
+	public WebElement getVerifyName() {
+		return verifyName;
+	}
+
+	// Logic
+
+	public void clickSearch(WebDriver driver) {
+		search.click();
+	}
+
+	public void enterEmployeeName(String emp) {
+		employee_Name.sendKeys(emp);
+	}
+
+	public void verifyEmployeeName(String emp) {
+
+		String empName = verifyName.getText();
+		if (emp.contains(empName))
+			System.out.println("Employee Verified  ....PASS");
+
+		else
+			System.out.println("Employee Not Found ....FAIL");
+	}
+
 }
